11e4a4d33e6e6f5cc01642712f90c502af01086c
vinitaran <vinitarane1998@gmail.com>
2020-10-30 01:45:29 +0530

    added hover images

--- Oct/main.js
+++ Oct/main.js
@@ -19,11 +19,11 @@ function checkMode() {
 }
 
 function darkModeOn() {
-  document.body.classList.add("dark-mode");
+  document.body.classList.add("light-mode");
 }
 
 function darkModeOff() {
-  document.body.classList.remove("dark-mode");
+  document.body.classList.remove("light-mode");
 }
 
 // function update() {
